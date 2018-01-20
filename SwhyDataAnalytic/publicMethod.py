03a67450d66d7d49760157766139e0f58c11c4fa
longmumin <longmumin@gmail.com>
2018-01-21 00:32:28 +0800

    下拉框公共方法完成

--- SwhyDataAnalytic/publicMethod.py
+++ SwhyDataAnalytic/publicMethod.py
@@ -14,11 +14,26 @@ def getSysCode(request):
     cursor = connection.cursor()
     # 查询数据
     try:
-        cursor.execute("select sys_code.code, sys_code.codename from sys_code where codetype = %s", codeType)
+        cursor.execute("select sys_code.code, sys_code.codename from sys_code where codetype = %s", [codeType])
     except Exception as e:
         print("select table failed, ret = %s" % e.args[0])
         cursor.close()
 
     codeData = cursor.fetchall()
+    cursor.close()
+    keys = ['val', 'text']
+    codeData = list2dict(keys, codeData)
     print(codeData)
     return JsonResponse(json.dumps(codeData, ensure_ascii=False, sort_keys=True), safe=False)
+
+
+def list2dict(keys, values):
+    dictData = []
+    for value in values:
+        row = {}
+        value = list(value)
+        for i in range(0, len(keys)):
+            row[keys[i]] = str(value[i])
+        #时间戳作为keys
+        dictData.append(row)
+    return dictData
